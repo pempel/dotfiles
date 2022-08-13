d65c9d7840822258b5bac2fa72b8aada3abe13d1
Eugene Pempel <eugene.pempel@gmail.com>
2022-08-13 15:59:56 +1200

    Fix a few dev icons

--- nvim/.config/nvim/lua/user/nvim-web-devicons.lua
+++ nvim/.config/nvim/lua/user/nvim-web-devicons.lua
@@ -3,12 +3,23 @@ if not status_ok then
   return
 end
 
+local function conf_icon(name)
+  return {
+    icon = '',
+    color = '#6d8086',
+    name = name
+  }
+end
+
 nvim_web_devicons.setup({
   override = {
-    Brewfile = {
+    ['Brewfile'] = {
       icon = '',
       color = '#f0b357',
       name = 'Brewfile'
-    }
+    },
+    ['.zshrc'] = conf_icon('Zshrc'),
+    ['skhdrc'] = conf_icon('Skhdrc'),
+    ['ripgreprc'] = conf_icon('Ripgreprc')
   }
 })
