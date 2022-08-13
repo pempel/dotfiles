2e8c0dc89430b5a0d04121e7127ba51914512103
Eugene Pempel <eugene.pempel@gmail.com>
2022-08-13 15:34:25 +1200

    Move .tmux.conf to tmux/.config/tmux

--- nvim/.config/nvim/lua/user/nvim-tree.lua
+++ nvim/.config/nvim/lua/user/nvim-tree.lua
@@ -9,8 +9,13 @@ nvim_tree.setup({
     width = 40,
     mappings = {
       list = {
-        { key = 'x', action = 'split' },
-        { key = 'v', action = 'vsplit' },
+        { key = '<c-x>', action = 'split' },
+        { key = '<c-v>', action = 'vsplit' },
+        { key = 'a', action = 'create' },
+        { key = 'd', action = 'remove' },
+        { key = 'x', action = 'cut' },
+        { key = 'c', action = 'copy' },
+        { key = 'p', action = 'paste' },
         { key = 'e', action = 'expand_all' }
       }
     }
